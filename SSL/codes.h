b1bc3d26719e9c6d69da89f3ea4ca0bd92cb0b72
Project-Luxy <96788441+Project-Luxy@users.noreply.github.com>
2022-02-18 16:31:18 +0100

    Create codes.h

---
+++ SSL/codes.h
@@ -0,0 +1,20 @@
+#pragma once
+
+// Error codes
+/*
+0-99:
+Minhook Errors
+100-199:
+C Errors
+200-299:
+Kiero Errors
+300-399
+*/
+
+namespace
+{
+	int BOOL_TO_STR_FAILED = 100;
+	int FSTREAM_NOT_OPEN = 200;
+	int KIERO_NOT_INITIALIZED = 300;
+	int KIERO_UNKNOWN_ERROR = 399;
+}
